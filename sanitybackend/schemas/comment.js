32c8eaaca11d2d0271ae3be06a33cfd5f7096dab
Tomasz Rogala <tomasz.rogala1505@gmail.com>
2022-07-07 11:33:34 +0200

    ci: implement sanity

---
+++ sanitybackend/schemas/comment.js
@@ -0,0 +1,17 @@
+export default {
+  name: "comment",
+  title: "Comment",
+  type: "document",
+  fields: [
+    {
+      name: "posedBy",
+      title: "PosedBy",
+      type: "postedBy",
+    },
+    {
+      name: "comment",
+      title: "Comment",
+      type: "string",
+    },
+  ],
+};
